31ce32ca695df2e9a7b7d86b8a4d274d21d3b533
mykyta <limeniye@gmail.com>
2023-08-04 10:59:15 +0300

    Created FlexibleItemsControl

---
+++ src/ui/TerritoryDefence.UI.CustomControls/FlexibleItemsControl/FlexibleItemsControl.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace TerritoryDefence.UI.CustomControls.FlexibleItemsControl;
+
+public partial class FlexibleItemsControl : ItemsControl
+{
+	public FlexibleItemsControl() : base()
+	{
+		this.DefaultStyleKey = typeof(FlexibleItemsControl);
+	}
+}
