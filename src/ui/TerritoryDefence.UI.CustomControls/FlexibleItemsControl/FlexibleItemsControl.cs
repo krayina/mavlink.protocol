c703b734cdd7fb62ad216bb7a0286ea402b05761
mykyta <limeniye@gmail.com>
2023-08-05 20:58:44 +0300

    [Temporary] FlexibleItemsControl ContentPresenter is works from RelativeSourceBinding

--- src/ui/TerritoryDefence.UI.CustomControls/FlexibleItemsControl/FlexibleItemsControl.cs
+++ src/ui/TerritoryDefence.UI.CustomControls/FlexibleItemsControl/FlexibleItemsControl.cs
@@ -45,8 +45,9 @@ public partial class FlexibleItemsControl : ItemsControl
 	protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
 	{
 		ContentPresenter defaultContentPresenter = element as ContentPresenter;
-		base.PrepareContainerForItemOverride(new ContentControl(), item);
-		//defaultContentPresenter.ContentTemplate = RelativeModeTemplate;
+
+		base.PrepareContainerForItemOverride(element, item);
+		defaultContentPresenter.ContentTemplate = RelativeModeTemplate;
 
 		//var relativeModeTemplateContent = RelativeModeTemplate.LoadContent() as FrameworkElement;
 		//var contentPresenter = FindVisualChild<ContentPresenter>(relativeModeTemplateContent);
@@ -152,3 +153,74 @@ public partial class FlexibleItemsControl : ItemsControl
 		}
 	}
 }
+
+
+public class RelativeSourceBinding
+{
+	public static readonly DependencyProperty AncestorTypeProperty = DependencyProperty.RegisterAttached("AncestorType", typeof(Type), typeof(RelativeSourceBinding), new PropertyMetadata(default(Type), OnAncestorTypeChanged));
+
+	public static void SetAncestorType(DependencyObject element, Type value)
+	{
+		element.SetValue(AncestorTypeProperty, value);
+	}
+
+	public static Type GetAncestorType(DependencyObject element)
+	{
+		return (Type)element.GetValue(AncestorTypeProperty);
+	}
+
+	private static void OnAncestorTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+	{
+		((FrameworkElement)d).Loaded -= OnFrameworkElementLoaded;
+
+		if (e.NewValue != null)
+		{
+			((FrameworkElement)d).Loaded += OnFrameworkElementLoaded;
+			OnFrameworkElementLoaded((FrameworkElement)d, null);
+		}
+	}
+
+	private static void OnFrameworkElementLoaded(object sender, RoutedEventArgs? e)
+	{
+		var ancestorType = GetAncestorType((FrameworkElement)sender);
+		if (ancestorType != null)
+		{
+			var findAncestor = ((FrameworkElement)sender).FindVisualParent(ancestorType);
+			RelativeSourceBinding.SetAncestor(((FrameworkElement)sender), findAncestor);
+		}
+		else
+		{
+			RelativeSourceBinding.SetAncestor(((FrameworkElement)sender), null);
+		}
+	}
+
+	public static readonly DependencyProperty AncestorProperty = DependencyProperty.RegisterAttached("Ancestor", typeof(UIElement), typeof(RelativeSourceBinding), new PropertyMetadata(default(FrameworkElement)));
+
+	public static void SetAncestor(DependencyObject element, UIElement? value)
+	{
+		element.SetValue(AncestorProperty, value);
+	}
+
+	public static UIElement GetAncestor(DependencyObject element)
+	{
+		return (UIElement)element.GetValue(AncestorProperty);
+	}
+}
+
+public static class Aa
+{
+	public static UIElement FindVisualParent(this UIElement element, Type type)
+	{
+		UIElement parent = element;
+		while (parent != null)
+		{
+			if (type.IsAssignableFrom(parent.GetType()))
+			{
+				return parent;
+			}
+			parent = VisualTreeHelper.GetParent(parent) as UIElement;
+		}
+		return null;
+	}
+}
+
