d2bb4c97ed5c907b2ff16b4b344f6ad998018d9b
mykyta <limeniye@gmail.com>
2023-08-06 18:00:08 +0300

    Adding a two element to FlexibleItemsControl

--- src/ui/TerritoryDefence.UI.CustomControls/FlexibleItemsControl/FlexibleItemsControl.cs
+++ src/ui/TerritoryDefence.UI.CustomControls/FlexibleItemsControl/FlexibleItemsControl.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media;
 
 namespace TerritoryDefence.UI.CustomControls.FlexibleItemsControl;
@@ -17,7 +18,7 @@ public partial class FlexibleItemsControl : ItemsControl
 					default,
 					FrameworkPropertyMetadataOptions.ValueDoesNotInheritDataContext
 #endif
-					//(s, e) => ((FlexibleItemsControl)s)?.OnRelativeModeTemplateChanged((DataTemplate)e.OldValue, (DataTemplate)e.NewValue)
+				//(s, e) => ((FlexibleItemsControl)s)?.OnRelativeModeTemplateChanged((DataTemplate)e.OldValue, (DataTemplate)e.NewValue)
 				)
 			);
 
@@ -35,8 +36,49 @@ public partial class FlexibleItemsControl : ItemsControl
 	protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
 	{
 		ContentPresenter defaultContentPresenter = element as ContentPresenter;
-
 		base.PrepareContainerForItemOverride(element, item);
 		defaultContentPresenter.ContentTemplate = RelativeModeTemplate;
+		SetDefaultRelativePosition(defaultContentPresenter);
+	}
+
+	protected virtual void SetDefaultRelativePosition(ContentPresenter container)
+	{
+		if (Items.Count == 1)
+		{
+			SetRelativeFullSize(container);
+		}
+		if (Items.Count == 2)
+		{
+			var currentIndex = IndexFromContainer(container);
+			SetRelativeUnderPrevious(container, (ContentPresenter)ContainerFromIndex(currentIndex - 1));
+		}
+	}
+
+	class RelativeContainerInfo
+	{
+		public ContentPresenter ContentPresenter { get; }
+	}
+
+	private void SetRelativeFullSize(ContentPresenter container)
+	{
+		container.SetValue(RelativePanel.AlignTopWithPanelProperty, true);
+		container.SetValue(RelativePanel.AlignLeftWithPanelProperty, true);
+
+		container.SetValue(RelativePanel.AlignBottomWithPanelProperty, true);
+		container.SetValue(RelativePanel.AlignRightWithPanelProperty, true);
+	}
+
+	private void SetRelativeUnderPrevious(ContentPresenter container, ContentPresenter previousContainer)
+	{
+		previousContainer.SetValue(RelativePanel.AlignBottomWithPanelProperty, false);
+		previousContainer.Height = ActualHeight / 2;
+
+		//container.SetValue(RelativePanel.AlignTopWithPanelProperty, true);
+		container.SetValue(RelativePanel.AlignLeftWithPanelProperty, true);
+
+		container.SetValue(RelativePanel.AlignBottomWithPanelProperty, true);
+		container.SetValue(RelativePanel.AlignRightWithPanelProperty, true);
+
+		container.SetValue(RelativePanel.BelowProperty, previousContainer);
 	}
 }
