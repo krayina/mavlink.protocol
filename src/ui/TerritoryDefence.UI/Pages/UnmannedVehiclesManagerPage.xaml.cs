33dc7eef17b06e2f8c6290bff9358a457148374f
mykyta <limeniye@gmail.com>
2023-08-07 18:16:51 +0300

    Created UnmannedAerialVehicle UserControl

--- src/ui/TerritoryDefence.UI/Pages/UnmannedVehiclesManagerPage.xaml.cs
+++ src/ui/TerritoryDefence.UI/Pages/UnmannedVehiclesManagerPage.xaml.cs
@@ -1,16 +1,9 @@
-﻿using CommunityToolkit.WinUI.UI.Controls;
-using Mapsui.Tiling;
-
-namespace TerritoryDefence.UI.Pages;
+﻿namespace TerritoryDefence.UI.Pages;
 
 public sealed partial class UnmannedVehiclesManagerPage : Page
 {
 	public UnmannedVehiclesManagerPage()
 	{
 		this.InitializeComponent();
-		//MapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
-		//MapControl.Map.Navigator.RotationLock = false;
-		//MapControl.UnSnapRotationDegrees = 30;
-		//MapControl.ReSnapRotationDegrees = 5;
 	}
 }
---
+++ src/ui/TerritoryDefence.UI/UserControls/UnmannedVehicles/UnmannedAerialVehicle.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Mapsui.Tiling;
+
+namespace TerritoryDefence.UI.UserControls;
+public sealed partial class UnmannedAerialVehicle : UserControl
+{
+	public UnmannedAerialVehicle()
+	{
+		this.InitializeComponent();
+		MapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+		MapControl.Map.Navigator.RotationLock = false;
+		MapControl.UnSnapRotationDegrees = 30;
+		MapControl.ReSnapRotationDegrees = 5;
+	}
+}
