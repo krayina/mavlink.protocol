537cbf2443dfd62e22fdb6d897fffbf727217e13
mykyta <limeniye@gmail.com>
2023-08-07 16:01:03 +0300

    Created ShowAttachedFlyoutBehavior

---
+++ src/ui/TerritoryDefence.UI.Toolkit/Behaviors/ShowAttachedFlyoutBehavior.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.WinUI.UI.Behaviors;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls.Primitives;
+
+namespace TerritoryDefence.UI.Toolkit.Behaviors;
+
+public class ShowAttachedFlyoutBehavior
+	: BehaviorBase<FrameworkElement>
+{
+	protected override bool Initialize()
+	{
+		var result = ShowAttachedFlyout();
+		return result;
+	}
+
+	protected override bool Uninitialize()
+	{
+		HideAttachedFlyout();
+		return true;
+	}
+
+	private bool ShowAttachedFlyout()
+	{
+		if (AssociatedObject == null)
+		{
+			return false;
+		}
+		FlyoutBase.ShowAttachedFlyout(AssociatedObject);
+		return true;
+	}
+
+	private void HideAttachedFlyout()
+	{
+		FlyoutBase.GetAttachedFlyout(AssociatedObject).Hide();
+	}
+}
