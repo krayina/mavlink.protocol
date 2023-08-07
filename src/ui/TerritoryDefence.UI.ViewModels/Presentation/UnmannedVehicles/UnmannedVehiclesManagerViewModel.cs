7402ed65cf4919bdb9f05e77af727b0e040584e8
mykyta <limeniye@gmail.com>
2023-08-07 18:07:58 +0300

    CreateUnmannedAerialVehicle test command

--- src/ui/TerritoryDefence.UI.ViewModels/Presentation/UnmannedVehicles/UnmannedVehiclesManagerViewModel.cs
+++ src/ui/TerritoryDefence.UI.ViewModels/Presentation/UnmannedVehicles/UnmannedVehiclesManagerViewModel.cs
@@ -1,20 +1,16 @@
 ﻿using System.Collections.ObjectModel;
 using System.Reactive;
-using DynamicData;
 using ReactiveUI;
 
 namespace TerritoryDefence.UI.ViewModels.Presentation.UnmannedVehicles;
 
 public class UnmannedVehiclesManagerViewModel
 {
-	public ObservableCollection<string> UnmannedVehicles { get; } = new()
-	{
-		//{ "0" }, { "1" }, { "2" }, { "3" }, { "4" }
-	};
+	public ObservableCollection<string> UnmannedVehicles { get; } = new();
 
-	public ReactiveCommand<Unit, Unit> CreateUnmannedVehicle => ReactiveCommand.Create(CreateUnmannedVehicleExecute);
+	public ReactiveCommand<Unit, Unit> CreateUnmannedAerialVehicleCommand => ReactiveCommand.Create(CreateUnmannedAerialVehicleExecute);
 
-	private void CreateUnmannedVehicleExecute()
+	private void CreateUnmannedAerialVehicleExecute()
 	{
 		int newValue = UnmannedVehicles.Count + 1;
 		UnmannedVehicles.Add(newValue.ToString(Thread.CurrentThread.CurrentCulture));
