b6f4fb269dd256d5fb552dcad768fcbbd842253a
mykyta <limeniye@gmail.com>
2023-08-10 18:12:46 +0300

    Created MoveElementPositionBehavior

---
+++ src/ui/TerritoryDefence.UI.Toolkit/Behaviors/MoveElementPositionBehavior.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.Xaml.Interactivity;
+
+namespace TerritoryDefence.UI.Toolkit.Behaviors;
+
+public class MoveElementPositionBehavior : Behavior<FrameworkElement>
+{
+	protected override void OnAttached()
+	{
+		base.OnAttached();
+	}
+
+	protected override void OnDetaching()
+	{
+		base.OnDetaching();
+	}
+}
