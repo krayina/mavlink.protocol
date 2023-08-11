8bf3e13ce0edfa1528fbd06a70808249ccd6b63f
mykyta <limeniye@gmail.com>
2023-08-11 11:54:17 +0300

    FrameworkElementExtensions InitializeTransform

---
+++ src/ui/TerritoryDefence.UI.Toolkit/Extensions/FrameworkElementExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+
+namespace TerritoryDefence.UI.Toolkit.Extensions;
+
+public static class FrameworkElementExtensions
+{
+	public static TranslateTransform InitializeTransform(this FrameworkElement element)
+	{
+		var renderTransform = element.RenderTransform as TranslateTransform;
+
+		if (renderTransform is null)
+		{
+			renderTransform = new TranslateTransform();
+			element.RenderTransform = renderTransform;
+		}
+		return renderTransform;
+	}
+}
