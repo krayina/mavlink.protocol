26086036d1921aef9ac95fd7feaeb52116fad806
mykyta <limeniye@gmail.com>
2023-08-11 13:19:08 +0300

    Added GetTopUIElement method

--- src/ui/TerritoryDefence.UI.Toolkit/Extensions/UIElementExtensions.cs
+++ src/ui/TerritoryDefence.UI.Toolkit/Extensions/UIElementExtensions.cs
@@ -5,9 +5,9 @@ namespace TerritoryDefence.UI.Toolkit.Extensions;
 
 public static class UIElementExtensions
 {
-	public static UIElement FindVisualParent(this UIElement element, Type type)
+	public static UIElement? FindVisualParent(this UIElement element, Type type)
 	{
-		UIElement parent = element;
+		UIElement? parent = element;
 		while (parent != null)
 		{
 			if (type.IsAssignableFrom(parent.GetType()))
@@ -17,5 +17,15 @@ public static class UIElementExtensions
 			parent = VisualTreeHelper.GetParent(parent) as UIElement;
 		}
 		return null;
+	}
+
+	public static UIElement? GetTopUIElement(this DependencyObject element)
+	{
+		DependencyObject? parent;
+		while ((parent = VisualTreeHelper.GetParent(element)) != null)
+		{
+			element = parent;
+		}
+		return element as UIElement;
 	}
 }
