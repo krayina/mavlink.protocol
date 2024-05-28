8043cb0900d347dbf6fbd74a187250639de27197
limeniye <limeniye@gmail.com>
2024-05-28 15:49:15 +0300

    chore(test): ProtocolGenerator_GenerateAllTypes_Verify

---
+++ src/mavlink/Shmyndra.Mavlink.SourceGenerators.Tests/Unit/MavlinkGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using Shmyndra.Mavlink.SourceGenerators.Protocol;
+
+namespace Shmyndra.Mavlink.SourceGenerators.Tests.Unit;
+
+public class MavlinkGeneratorTests
+{
+	[Fact]
+	public Task ProtocolGenerator_GenerateAllTypes_Verify()
+	{
+		// arrange
+		var generator = new MavlinkGenerator();
+
+		var additional = TestsHelper.GetAdditionalTextList([
+			"Stubs\\test-mavlink-common.xml",
+			"Stubs\\test-mavlink-minimal.xml",
+			"Stubs\\test-mavlink-standard.xml"
+		]);
+
+		// act
+		var driver = generator.RunIncrementalGeneratorDriver(additional);
+		var runResult = driver.GetRunResult().Results.Single();
+		var generatedCode = string.Join(Environment.NewLine, runResult.GeneratedSources.Select(source => source.SourceText.ToString()));
+
+		// assert
+		return Verify(generatedCode).UseDirectory("..\\Snapshots");
+	}
+}
