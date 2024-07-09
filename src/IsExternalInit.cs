d626722f89bed7dcfc41f625751dcb6fff5785bc
limeniye <limeniye@gmail.com>
2024-07-09 15:00:22 +0300

    chore: Add PolySharp extensions for netstandard2.0 projects

--- src/IsExternalInit.cs
+++
@@ -1,7 +0,0 @@
-﻿#if NETSTANDARD2_0_OR_GREATER
-using System.ComponentModel;
-
-namespace System.Runtime.CompilerServices;
-[EditorBrowsable(EditorBrowsableState.Never)]
-public record IsExternalInit;
-#endif
