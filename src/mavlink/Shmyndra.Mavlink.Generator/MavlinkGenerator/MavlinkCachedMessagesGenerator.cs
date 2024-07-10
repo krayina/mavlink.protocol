e0d2c0e7319970bcdfe5f7376509565fc5724696
limeniye <limeniye@gmail.com>
2024-07-10 12:45:36 +0300

    feat: Extend MavlinkMessages with TryGet methods and get by id

--- src/mavlink/Shmyndra.Mavlink.Generator/MavlinkGenerator/MavlinkCachedMessagesGenerator.cs
+++ src/mavlink/Shmyndra.Mavlink.Generator/MavlinkGenerator/MavlinkCachedMessagesGenerator.cs
@@ -3,31 +3,77 @@ using Microsoft.CodeAnalysis.CSharp;
 
 namespace Shmyndra.Mavlink.Generator;
 
-internal record MavlinkCachedMessage(string FullName, ulong Id, string XmlName);
+internal record MavlinkCachedMessage(string FullName, uint Id, string XmlName);
 
 internal static class MavlinkCachedMessagesGenerator
 {
 	public static MemberDeclarationSyntax GenerateMessagesCache(IEnumerable<MavlinkCachedMessage> messages)
 	{
-		var dictionaryEntries = string.Join(",\n", messages.Select(message =>
+		var dictionaryEntriesByType = string.Join(",\n", messages.Select(message =>
 			$"{{ typeof({message.FullName}), ({message.Id}U, \"{message.XmlName}\") }}"));
+
+		var dictionaryEntriesById = string.Join(",\n", messages.Select(message =>
+			$"{{ {message.Id}U, (typeof({message.FullName}), \"{message.XmlName}\") }}"));
 
 		var classCode = $@"
 public static class MavlinkMessages
 {{
-    private static Dictionary<Type, (ulong Id, string XmlName)> _mavlinkMessages = new()
+    private static readonly Dictionary<Type, (uint Id, string XmlName)> _mavlinkMessagesByType = new()
     {{
-        {dictionaryEntries}
+        {dictionaryEntriesByType}
     }};
 
-    public static ulong GetId<T>() where T : MavlinkMessage
+    private static readonly Dictionary<uint, (Type Type, string XmlName)> _mavlinkMessagesById = new()
     {{
-        return _mavlinkMessages[typeof(T)].Id;
+        {dictionaryEntriesById}
+    }};
+
+    public static uint GetId<T>() where T : MavlinkMessage
+    {{
+        return _mavlinkMessagesByType[typeof(T)].Id;
     }}
 
     public static string GetXmlName<T>() where T : MavlinkMessage
     {{
-        return _mavlinkMessages[typeof(T)].XmlName;
+        return _mavlinkMessagesByType[typeof(T)].XmlName;
+    }}
+
+    public static string GetXmlName(uint id)
+    {{
+        return _mavlinkMessagesById[id].XmlName;
+    }}
+
+    public static Type GetType(uint id)
+    {{
+        return _mavlinkMessagesById[id].Type;
+    }}
+
+    public static bool TryGetId<T>(out uint id) where T : MavlinkMessage
+    {{
+        var isExists = _mavlinkMessagesByType.TryGetValue(typeof(T), out var value);
+        id = isExists ? value.Id : default;
+        return isExists;
+    }}
+
+    public static bool TryGetXmlName<T>(out string xmlName) where T : MavlinkMessage
+    {{
+        var isExists = _mavlinkMessagesByType.TryGetValue(typeof(T), out var value);
+        xmlName = isExists ? value.XmlName : default;
+        return isExists;
+    }}
+
+    public static bool TryGetXmlName(uint id, out string xmlName)
+    {{
+        var isExists = _mavlinkMessagesById.TryGetValue(id, out var value);
+        xmlName = isExists ? value.XmlName : default;
+        return isExists;
+    }}
+
+    public static bool TryGetType(uint id, out Type type)
+    {{
+        var isExists = _mavlinkMessagesById.TryGetValue(id, out var value);
+        type = isExists ? value.Type : default;
+        return isExists;
     }}
 }}";
 
