768b1f2f6e96d50b913d0d46caf0f2164fa53770
limeniye <limeniye@gmail.com>
2024-07-29 16:40:37 +0300

    chore: Add GeneratedMavlinkMessageField

---
+++ src/mavlink/Shmyndra.Mavlink.Generator/Data/GeneratedMavlinkMessageField.cs
@@ -0,0 +1,61 @@
+﻿namespace Shmyndra.Mavlink.Generator;
+
+/// <summary>
+/// Represents a Mavlink message field that has been generated with an additional generated name.
+/// </summary>
+/// <remarks>
+/// This type inherits from <see cref="MavlinkMessageField"/> and includes an additional property <see cref="GeneratedName"/>.
+/// The <see cref="GeneratedName"/> property contains a modified version of the <see cref="MavlinkMessageField.Name"/> property,
+/// which reflects the name of the field as it appears in the generated code.
+/// </remarks>
+public record GeneratedMavlinkMessageField : MavlinkMessageField
+{
+	/// <summary>
+	/// Gets the generated name of the field.
+	/// </summary>
+	/// <remarks>
+	/// The <see cref="GeneratedName"/> represents the field name after it has been processed
+	/// and modified for code generation purposes. This might include transformations such as
+	/// converting to camelCase, adding prefixes or suffixes, or other modifications to make
+	/// the name valid or more readable in the target programming language.
+	/// </remarks>
+	public string GeneratedName { get; init; }
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="GeneratedMavlinkMessageField"/> record.
+	/// </summary>
+	/// <param name="generatedName">The generated name of the field, derived from the original name.</param>
+	/// <param name="type">The type of the field.</param>
+	/// <param name="name">The name of the field.</param>
+	/// <param name="description">The description of the field.</param>
+	/// <param name="display">The display information of the field.</param>
+	/// <param name="systemUnit">The system unit of the field.</param>
+	/// <param name="isRequired">Indicates whether the field is required.</param>
+	/// <param name="printFormat">The print format of the field.</param>
+	/// <param name="increment">The increment step for the field value.</param>
+	/// <param name="minValue">The minimum value for the field.</param>
+	/// <param name="maxValue">The maximum value for the field.</param>
+	/// <param name="instance">A value indicating whether the field is instance-specific.</param>
+	/// <param name="default">The default value for the field.</param>
+	/// <param name="invalid">The invalid value for the field.</param>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="name"/> or <paramref name="generatedName"/> is <c>null</c>.</exception>
+	public GeneratedMavlinkMessageField(
+		string generatedName,
+		MavlinkMessageFieldType type,
+		string name,
+		string? description,
+		MavlinkMessageFieldDisplay display,
+		MavlinkSystemUnit systemUnit,
+		bool isRequired,
+		string? printFormat,
+		float? increment,
+		float? minValue,
+		float? maxValue,
+		bool? instance,
+		string? @default,
+		string? invalid)
+		: base(type, name, description, display, systemUnit, isRequired, printFormat, increment, minValue, maxValue, instance, @default, invalid)
+	{
+		GeneratedName = generatedName ?? throw new ArgumentNullException(nameof(generatedName));
+	}
+}
---
+++ src/mavlink/Shmyndra.Mavlink.Generator/Data/GeneratedTypesExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Shmyndra.Mavlink.Generator;
+
+/// <summary>
+/// Provides extension methods for converting Mavlink message fields to their generated counterparts.
+/// </summary>
+public static class GeneratedTypesExtensions
+{
+	/// <summary>
+	/// Converts a <see cref="MavlinkMessageField"/> to a <see cref="GeneratedMavlinkMessageField"/> with a specified generated name.
+	/// </summary>
+	/// <param name="field">The original Mavlink message field.</param>
+	/// <param name="generatedName">The generated name for the new field.</param>
+	/// <returns>A new instance of <see cref="GeneratedMavlinkMessageField"/> with the specified generated name.</returns>
+	public static GeneratedMavlinkMessageField ToGeneratedMavlinkMessageField(this MavlinkMessageField field, string generatedName)
+	{
+		if (field == null)
+		{
+			throw new ArgumentNullException(nameof(field));
+		}
+
+		return new GeneratedMavlinkMessageField(
+			generatedName: generatedName,
+			type: field.Type,
+			name: field.Name,
+			description: field.Description,
+			display: field.Display,
+			systemUnit: field.SystemUnit,
+			isRequired: field.IsRequired,
+			printFormat: field.PrintFormat,
+			increment: field.Increment,
+			minValue: field.MinValue,
+			maxValue: field.MaxValue,
+			instance: field.Instance,
+			@default: field.Default,
+			invalid: field.Invalid
+		);
+	}
+}
