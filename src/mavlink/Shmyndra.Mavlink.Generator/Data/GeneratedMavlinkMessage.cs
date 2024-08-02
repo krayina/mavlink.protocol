0787fe925148963373f2198791f8a672ca5ac43e
limeniye <limeniye@gmail.com>
2024-08-02 11:57:30 +0300

    chore: Add GeneratedName and DeclarationSyntax for GeneratedMavlinkMessage

--- src/mavlink/Shmyndra.Mavlink.Generator/Data/GeneratedMavlinkMessage.cs
+++ src/mavlink/Shmyndra.Mavlink.Generator/Data/GeneratedMavlinkMessage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Shmyndra.Mavlink.Generator;
 
@@ -17,21 +18,41 @@ public record GeneratedMavlinkMessage : MavlinkMessage
 	public string GeneratedNamespace { get; init; }
 
 	/// <summary>
+	/// The generated name of the Mavlink message.
+	/// </summary>
+	public string GeneratedName { get; init; }
+
+	/// <summary>
 	/// The list of generated fields in the Mavlink message.
 	/// </summary>
 	public ImmutableArray<GeneratedMavlinkMessageField> GeneratedFields { get; init; }
 
 	/// <summary>
+	/// The declaration syntax of the generated message.
+	/// This syntax represents the C# code structure for the message.
+	/// </summary>
+	public RecordDeclarationSyntax DeclarationSyntax { get; init; }
+
+	/// <summary>
 	/// Initializes a new instance of the <see cref="GeneratedMavlinkMessage"/> record.
 	/// </summary>
 	/// <param name="generatedNamespace">The namespace associated with the generated message.</param>
+	/// <param name="generatedName">The generated name of the Mavlink message.</param>
 	/// <param name="generatedFields">The list of generated fields for the message.</param>
+	/// <param name="declarationSyntax">The syntax declaration of the generated message.</param>
 	/// <param name="originalMessage">The original Mavlink message.</param>
-	/// <exception cref="ArgumentNullException">Thrown when <paramref name="generatedNamespace"/> or <paramref name="generatedFields"/> is <c>null</c>.</exception>
-	internal GeneratedMavlinkMessage(string generatedNamespace, ImmutableArray<GeneratedMavlinkMessageField> generatedFields, MavlinkMessage originalMessage)
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="generatedNamespace"/>, <paramref name="generatedName"/>, or <paramref name="generatedFields"/> is <c>null</c>.</exception>
+	internal GeneratedMavlinkMessage(
+		string generatedNamespace,
+		string generatedName,
+		ImmutableArray<GeneratedMavlinkMessageField> generatedFields,
+		RecordDeclarationSyntax declarationSyntax,
+		MavlinkMessage originalMessage)
 		: base(originalMessage)
 	{
 		GeneratedNamespace = generatedNamespace ?? throw new ArgumentNullException(nameof(generatedNamespace));
+		GeneratedName = generatedName ?? throw new ArgumentNullException(nameof(generatedName));
 		GeneratedFields = generatedFields.IsDefault ? throw new ArgumentNullException(nameof(generatedFields)) : generatedFields;
+		DeclarationSyntax = declarationSyntax ?? throw new ArgumentNullException(nameof(declarationSyntax));
 	}
 }
