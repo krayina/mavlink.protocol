272776fbbb59353ddc2620740867af55fd97d085
limeniye <limeniye@gmail.com>
2024-08-14 16:12:38 +0300

    chore: Remove parent functionality from ReadOnlyMavlinkTree

--- src/mavlink/Shmyndra.Mavlink.Generator/XmlParser/ReadOnlyMavlinkTree.cs
+++ src/mavlink/Shmyndra.Mavlink.Generator/XmlParser/ReadOnlyMavlinkTree.cs
@@ -6,12 +6,10 @@ namespace Shmyndra.Mavlink.Generator;
 public class ReadOnlyMavlinkTree : IReadOnlyCollection<MavlinkFileNode>
 {
 	private readonly ImmutableHashSet<MavlinkFileNode> _nodes;
-	private readonly Dictionary<MavlinkFileNode, MavlinkFileNode?> _parentMap;
 
 	public ReadOnlyMavlinkTree(IEnumerable<MavlinkFileNode> nodes)
 	{
 		_nodes = nodes.ToImmutableHashSet();
-		_parentMap = BuildParentMap(_nodes);
 	}
 
 	public int Count => _nodes.Count;
@@ -19,15 +17,6 @@ public class ReadOnlyMavlinkTree : IReadOnlyCollection<MavlinkFileNode>
 	public IEnumerator<MavlinkFileNode> GetEnumerator() => _nodes.GetEnumerator();
 
 	IEnumerator IEnumerable.GetEnumerator() => _nodes.GetEnumerator();
-
-	public MavlinkFileNode? GetParent(MavlinkFileNode node)
-	{
-		if (_parentMap.TryGetValue(node, out var parentNode))
-		{
-			return parentNode;
-		}
-		return null;
-	}
 
 	public void ForEachTree(Action<MavlinkFileNode> action)
 	{
@@ -52,26 +41,5 @@ public class ReadOnlyMavlinkTree : IReadOnlyCollection<MavlinkFileNode>
 
 			action(node);
 		}
-	}
-
-	private Dictionary<MavlinkFileNode, MavlinkFileNode?> BuildParentMap(IEnumerable<MavlinkFileNode> nodes)
-	{
-		var parentMap = new Dictionary<MavlinkFileNode, MavlinkFileNode?>();
-
-		foreach (var node in nodes)
-		{
-			foreach (var include in node.Includes)
-			{
-				parentMap[include] = node;
-			}
-
-			if (!parentMap.ContainsKey(node))
-			{
-				// Root node
-				parentMap[node] = null;
-			}
-		}
-
-		return parentMap;
 	}
 }
