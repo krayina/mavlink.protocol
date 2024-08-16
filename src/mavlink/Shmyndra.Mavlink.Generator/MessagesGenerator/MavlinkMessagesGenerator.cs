d12de2a36771f116c66fe47e3a5cb62dee6ee27d
limeniye <limeniye@gmail.com>
2024-08-16 16:42:29 +0300

    chore: Update MavlinkTypes with CreateMessageInstance

--- src/mavlink/Shmyndra.Mavlink.Generator/MessagesGenerator/MavlinkMessagesGenerator.cs
+++ src/mavlink/Shmyndra.Mavlink.Generator/MessagesGenerator/MavlinkMessagesGenerator.cs
@@ -5,10 +5,10 @@ internal static class MavlinkMessagesGenerator
 	public static string GenerateMessageExtensions(IEnumerable<GeneratedMavlinkMessage> messages)
 	{
 		var dictionaryEntriesByType = string.Join(",\n", messages.Select(message =>
-			$"\t\t\t{{ typeof({message.GeneratedNamespace}.{message.GeneratedName}), ({message.Id}U, \"{message.Name}\") }}"));
+			$"\t\t\t{{ typeof({message.GeneratedNamespace}.{message.GeneratedName}), ({message.Id}U, \"{message.Name}\", payload => {message.GeneratedNamespace}.{message.GeneratedName}.CreateInstance(payload)) }}"));
 
 		var dictionaryEntriesById = string.Join(",\n", messages.Select(message =>
-			$"\t\t\t{{ {message.Id}U, (typeof({message.GeneratedNamespace}.{message.GeneratedName}), \"{message.Name}\") }}"));
+			$"\t\t\t{{ {message.Id}U, (typeof({message.GeneratedNamespace}.{message.GeneratedName}), \"{message.Name}\", payload => {message.GeneratedNamespace}.{message.GeneratedName}.CreateInstance(payload)) }}"));
 
 		var classCode =
 $@"using System;
@@ -18,12 +18,12 @@ namespace MavlinkTypes
 {{
     public static class MavlinkMessages
     {{
-        private static readonly Dictionary<Type, (uint Id, string MavlinkName)> _mavlinkMessagesByType = new()
+        private static readonly Dictionary<Type, (uint Id, string MavlinkName, Func<byte[], MavlinkMessage> Creator)> _mavlinkMessagesByType = new()
         {{
 {dictionaryEntriesByType}
         }};
 
-        private static readonly Dictionary<uint, (Type Type, string MavlinkName)> _mavlinkMessagesById = new()
+        private static readonly Dictionary<uint, (Type Type, string MavlinkName, Func<byte[], MavlinkMessage> Creator)> _mavlinkMessagesById = new()
         {{
 {dictionaryEntriesById}
         }};
@@ -46,6 +46,38 @@ namespace MavlinkTypes
         public static Type GetType(uint id)
         {{
             return _mavlinkMessagesById[id].Type;
+        }}
+
+        public static MavlinkMessage CreateMessageInstance(uint messageId, byte[] payload)
+        {{
+            return _mavlinkMessagesById[messageId].Creator(payload);
+        }}
+
+        public static MavlinkMessage CreateMessageInstance(Type messageType, byte[] payload)
+        {{
+            return _mavlinkMessagesByType[messageType].Creator(payload);
+        }}
+
+        public static bool TryCreateMessageInstance(uint messageId, byte[] payload, out MavlinkMessage message)
+        {{
+            if (_mavlinkMessagesById.TryGetValue(messageId, out var value))
+            {{
+                message = value.Creator(payload);
+                return true;
+            }}
+            message = default(MavlinkMessage);
+            return false;
+        }}
+
+        public static bool TryCreateMessageInstance(Type messageType, byte[] payload, out MavlinkMessage message)
+        {{
+            if (_mavlinkMessagesByType.TryGetValue(messageType, out var value))
+            {{
+                message = value.Creator(payload);
+                return true;
+            }}
+            message = default(MavlinkMessage);
+            return false;
         }}
 
         public static bool TryGetId<T>(out uint id) where T : MavlinkMessage
