566133fefabcc0e3965bde4fc385164aa10ff944
limeniye <limeniye@gmail.com>
2025-02-15 17:37:05 +0200

    feat: Add IMavlinkMessageSerializerWithoutExtensions and IMavlinkMessageSerializerWithExtensions

--- src/mavlink/Shmyndra.Mavlink/MavlinkMessage.cs
+++ src/mavlink/Shmyndra.Mavlink/MavlinkMessage.cs
@@ -14,3 +14,39 @@ public interface MavlinkMessage
 {
 	// The type should be left blank
 }
+
+/// <summary>
+/// Represents a serialization method for Mavlink messages that do not include extensions.
+/// Typically used for Mavlink v1 messages.
+/// </summary>
+/// <remarks>
+/// For more details on field reordering, see: 
+/// <a href="https://mavlink.io/en/guide/serialization.html#field_reordering">MAVLink Field Reordering</a>
+/// </remarks>
+public interface IMavlinkMessageSerializerWithoutExtensions : MavlinkMessage
+{
+	/// <summary>
+	/// Serializes the message into a byte array using the non-extension format.
+	/// This method is applicable to Mavlink v1 messages.
+	/// </summary>
+	/// <returns>A byte array containing the serialized message.</returns>
+	byte[] SerializeWithoutExtensions();
+}
+
+/// <summary>
+/// Represents a serialization method for Mavlink messages that include extensions.
+/// Typically used for Mavlink v2 messages.
+/// </summary>
+/// <remarks>
+/// For more details on message extensions, see: 
+/// <a href="https://mavlink.io/en/guide/define_xml_element.html#message_extensions">MAVLink Message Extensions</a>
+/// </remarks>
+public interface IMavlinkMessageSerializerWithExtensions : MavlinkMessage
+{
+	/// <summary>
+	/// Serializes the message into a byte array using the extension format.
+	/// This method is applicable to Mavlink v2 messages.
+	/// </summary>
+	/// <returns>A byte array containing the serialized message.</returns>
+	byte[] SerializeWithExtensions();
+}
