1756d412f498976841ee0e497cdfd6c6c8f24b5b
limeniye <limeniye@gmail.com>
2025-04-15 12:22:19 +0300

    chore: update usings

--- src/mavlink/Shmyndra.Mavlink.Generator/Utilities.cs
+++ src/mavlink/Shmyndra.Mavlink.Generator/Utilities.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+﻿using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System.Collections.Immutable;
 
 namespace Shmyndra.Mavlink.Generator;
 
