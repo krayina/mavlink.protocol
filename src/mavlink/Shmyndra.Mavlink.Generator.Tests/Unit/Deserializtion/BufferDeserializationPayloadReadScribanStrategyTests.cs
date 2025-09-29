ad160421f1d8784098c774e911ef7acb68d2c0cd
limeniye <limeniye@gmail.com>
2025-09-29 19:40:52 +0300

    test(deserializer): add unit tests for the deserialization generator

---
+++ src/mavlink/Shmyndra.Mavlink.Generator.Tests/Unit/Deserializtion/BufferDeserializationPayloadReadScribanStrategyTests.cs
@@ -0,0 +1,174 @@
+﻿using FluentAssertions;
+
+namespace Shmyndra.Mavlink.Generator.Tests.Unit.Deserializtion;
+
+public class BufferDeserializationPayloadReadScribanStrategyTests
+{
+	private readonly BufferDeserializationPayloadReadScribanStrategy _strategy;
+
+	public BufferDeserializationPayloadReadScribanStrategyTests()
+	{
+		_strategy = new BufferDeserializationPayloadReadScribanStrategy();
+	}
+
+	[Theory]
+	[InlineData("bool", 0, 2, "i")]
+	[InlineData("decimal", 4, 16, "idx")]
+	public void GenerateArrayElementReadExpression_UnsupportedType_ThrowsNotSupportedException(
+	string elementTypeName, int baseOffset, int elementSize, string indexVariable)
+	{
+		// Act
+		Action act = () => _strategy.GenerateArrayElementReadExpression(
+			elementTypeName, baseOffset, elementSize, indexVariable);
+
+		// Assert
+		act.Should().Throw<NotSupportedException>()
+			.WithMessage($"Unsupported type: {elementTypeName}");
+	}
+
+	[Theory]
+	[InlineData("byte", 0, 1, "payload[0]")]
+	[InlineData("byte", 10, 1, "payload[10]")]
+	[InlineData("sbyte", 0, 1, "(sbyte)payload[0]")]
+	[InlineData("sbyte", 5, 1, "(sbyte)payload[5]")]
+	public void GenerateScalarReadExpression_SingleByteTypes_ReturnsCorrectExpression(
+		string typeName, int offset, int size, string expected)
+	{
+		// Act
+		var result = _strategy.GenerateScalarReadExpression(typeName, offset, size);
+
+		// Assert
+		result.Should().Be(expected);
+	}
+
+	[Theory]
+	[InlineData("int", 0, 4, "System.BitConverter.ToInt32(payload, 0)")]
+	[InlineData("uint", 4, 4, "System.BitConverter.ToUInt32(payload, 4)")]
+	[InlineData("short", 8, 2, "System.BitConverter.ToInt16(payload, 8)")]
+	[InlineData("ushort", 10, 2, "System.BitConverter.ToUInt16(payload, 10)")]
+	[InlineData("long", 12, 8, "System.BitConverter.ToInt64(payload, 12)")]
+	[InlineData("ulong", 20, 8, "System.BitConverter.ToUInt64(payload, 20)")]
+	[InlineData("float", 28, 4, "System.BitConverter.ToSingle(payload, 28)")]
+	[InlineData("double", 32, 8, "System.BitConverter.ToDouble(payload, 32)")]
+	[InlineData("char", 40, 2, "System.BitConverter.ToChar(payload, 40)")]
+	public void GenerateScalarReadExpression_MultiByteTypes_ReturnsBitConverterCall(
+		string typeName, int offset, int size, string expected)
+	{
+		// Act
+		var result = _strategy.GenerateScalarReadExpression(typeName, offset, size);
+
+		// Assert
+		result.Should().Be(expected);
+	}
+
+	[Theory]
+	[InlineData("decimal", 16)]
+	[InlineData("bool", 1)]
+	[InlineData("string", 0)]
+	public void GenerateScalarReadExpression_UnsupportedTypes_ThrowsNotSupportedException(
+		string typeName, int size)
+	{
+		// Act
+		Action act = () => _strategy.GenerateScalarReadExpression(typeName, 0, size);
+
+		// Assert
+		act.Should().Throw<NotSupportedException>()
+			.WithMessage($"Unsupported type: {typeName}");
+	}
+
+	[Theory]
+	[InlineData("byte", 0, 1, "i", "payload[0 + i]")]
+	[InlineData("byte", 10, 1, "idx", "payload[10 + idx]")]
+	[InlineData("sbyte", 5, 1, "j", "(sbyte)payload[5 + j]")]
+	public void GenerateArrayElementReadExpression_SingleByteElements_ReturnsIndexedAccess(
+		string elementTypeName, int baseOffset, int elementSize, string indexVariable, string expected)
+	{
+		// Act
+		var result = _strategy.GenerateArrayElementReadExpression(
+			elementTypeName, baseOffset, elementSize, indexVariable);
+
+		// Assert
+		result.Should().Be(expected);
+	}
+
+	[Theory]
+	[InlineData("int", 0, 4, "i", "System.BitConverter.ToInt32(payload, 0 + i * 4)")]
+	[InlineData("ushort", 20, 2, "idx", "System.BitConverter.ToUInt16(payload, 20 + idx * 2)")]
+	[InlineData("float", 8, 4, "j", "System.BitConverter.ToSingle(payload, 8 + j * 4)")]
+	public void GenerateArrayElementReadExpression_MultiByteElements_CalculatesOffset(
+		string elementTypeName, int baseOffset, int elementSize, string indexVariable, string expected)
+	{
+		// Act
+		var result = _strategy.GenerateArrayElementReadExpression(
+			elementTypeName, baseOffset, elementSize, indexVariable);
+
+		// Assert
+		result.Should().Be(expected);
+	}
+
+	[Fact]
+	public void GenerateTerminatedStringReadBlock_CreatesCompleteCodeBlock()
+	{
+		// Arrange
+		var variableName = "testString";
+		var offset = 10;
+		var maxLength = 32;
+
+		// Act
+		var result = _strategy.GenerateTerminatedStringReadBlock(variableName, offset, maxLength);
+
+		// Assert
+		result.Should().Contain($"var {variableName}Terminator = System.Array.IndexOf(payload, (byte)0, {offset}, {maxLength});");
+		result.Should().Contain($"var {variableName}Length = {variableName}Terminator == -1 ? {maxLength} : {variableName}Terminator - {offset};");
+		result.Should().Contain($"var {variableName}String = System.Text.Encoding.ASCII.GetString(payload, {offset}, {variableName}Length);");
+		result.Should().Contain($"var {variableName} = System.Collections.Immutable.ImmutableArray.CreateRange({variableName}String);");
+	}
+
+	[Fact]
+	public void GenerateTerminatedStringReadBlock_HandlesEmptyStringCase()
+	{
+		// Arrange
+		var variableName = "emptyStr";
+		var offset = 0;
+		var maxLength = 10;
+
+		// Act
+		var result = _strategy.GenerateTerminatedStringReadBlock(variableName, offset, maxLength);
+
+		// Assert
+		result.Should().Contain("emptyStrTerminator == -1");
+		result.Should().Contain("System.Text.Encoding.ASCII.GetString");
+	}
+
+	[Fact]
+	public void GenerateTerminatedStringReadBlock_UsesCorrectVariableNaming()
+	{
+		// Arrange
+		var variableName = "myField";
+		var offset = 5;
+		var maxLength = 20;
+
+		// Act
+		var result = _strategy.GenerateTerminatedStringReadBlock(variableName, offset, maxLength);
+
+		// Assert
+		result.Should().Contain("myFieldTerminator");
+		result.Should().Contain("myFieldLength");
+		result.Should().Contain("myFieldString");
+		result.Should().Contain("var myField =");
+	}
+
+	[Theory]
+	[InlineData("int", 2)]
+	[InlineData("float", 3)]
+	[InlineData("char", 3)]
+	public void GenerateScalarReadExpression_ForUnsupportedMultiByteType_ThrowsNotSupportedException(
+		string typeName, int invalidSize)
+	{
+		// Act
+		Action act = () => _strategy.GenerateScalarReadExpression(typeName, 0, invalidSize);
+
+		// Assert
+		act.Should().Throw<NotSupportedException>();
+	}
+}
---
+++ src/mavlink/Shmyndra.Mavlink.Generator.Tests/Unit/Deserializtion/InvalidValueExpressionBuilderTests.cs
@@ -0,0 +1,142 @@
+﻿using FluentAssertions;
+
+namespace Shmyndra.Mavlink.Generator.Tests.Unit.Deserializtion;
+
+public class InvalidValueExpressionBuilderTests
+{
+	private readonly InvalidValueExpressionBuilder _builder = new();
+
+	[Fact]
+	public void BuildCondition_NaN_ForFloatType_UsesFloatIsNaN_Negated()
+	{
+		// Arrange
+		var type = new GeneratedMavlinkMessageFieldPrimitiveType("float", Original: null!);
+
+		// Act
+		var result = _builder.BuildCondition("val", "NaN", type);
+
+		// Assert
+		result.Should().Be("!float.IsNaN(val)");
+	}
+
+	[Fact]
+	public void BuildCondition_NaN_ForDoubleType_UsesDoubleIsNaN_Negated()
+	{
+		// Arrange
+		var type = new GeneratedMavlinkMessageFieldPrimitiveType("double", Original: null!);
+
+		// Act
+		var result = _builder.BuildCondition("x", "NaN", type);
+
+		// Assert
+		result.Should().Be("!double.IsNaN(x)");
+	}
+
+	[Fact]
+	public void BuildCondition_NaN_ForNonFloatingType_ThrowsFormatException_WithDescriptiveMessage()
+	{
+		// Arrange
+		var type = new GeneratedMavlinkMessageFieldPrimitiveType("int", Original: null!);
+
+		// Act
+		Action act = () => _builder.BuildCondition("v", "NaN", type);
+
+		// Assert
+		act.Should().Throw<FormatException>()
+		   .WithMessage("The invalid value 'NaN' is only applicable to floating-point types, but was used with 'int'.");
+	}
+
+	[Theory]
+	[InlineData("UINT8_MAX", "byte.MaxValue")]
+	[InlineData("UINT16_MAX", "ushort.MaxValue")]
+	[InlineData("UINT32_MAX", "uint.MaxValue")]
+	[InlineData("UINT64_MAX", "ulong.MaxValue")]
+	[InlineData("INT8_MAX", "sbyte.MaxValue")]
+	[InlineData("INT16_MAX", "short.MaxValue")]
+	[InlineData("INT32_MAX", "int.MaxValue")]
+	[InlineData("INT64_MAX", "long.MaxValue")]
+	public void BuildCondition_KnownMavlinkConstants_AreTranslatedToClrMaxValues(string rawInvalid, string expectedLiteral)
+	{
+		// Arrange
+		var type = new GeneratedMavlinkMessageFieldPrimitiveType("int", Original: null!);
+
+		// Act
+		var result = _builder.BuildCondition("value", rawInvalid, type);
+
+		// Assert
+		result.Should().Be($"value != {expectedLiteral}");
+	}
+
+	[Theory]
+	[InlineData("1", "1")]
+	[InlineData("-42", "-42")]
+	[InlineData("1.5", "1.5")]
+	[InlineData("1e-3", "0.001")]
+	public void BuildCondition_NumericLiteral_InvariantCulture_IsUsed(string literal, string expectedLiteral)
+	{
+		// Arrange
+		var type = new GeneratedMavlinkMessageFieldPrimitiveType("double", Original: null!);
+
+		// Act
+		var result = _builder.BuildCondition("x", literal, type);
+
+		// Assert
+		result.Should().Be($"x != {expectedLiteral}");
+	}
+
+	[Fact]
+	public void BuildCondition_NumericLiteral_CommaAsDecimalSeparator_IsRejected()
+	{
+		// Arrange
+		var type = new GeneratedMavlinkMessageFieldPrimitiveType("double", Original: null!);
+
+		// Act
+		Action act = () => _builder.BuildCondition("x", "1,5", type);
+
+		// Assert
+		act.Should().Throw<FormatException>();
+	}
+
+	[Fact]
+	public void BuildCondition_EnumField_WrapsVariableWithConvertedEnumTypeCast()
+	{
+		// Arrange
+		var enumType = new GeneratedMavlinkMessageFieldEnumType("ushort", GeneratedEnum: null!, Original: null!);
+
+		// Act
+		var result = _builder.BuildCondition("e", "65535", enumType);
+
+		// Assert
+		result.Should().Be("(ushort)e != 65535");
+	}
+
+	[Fact]
+	public void BuildCondition_UnknownToken_ThrowsFormatException()
+	{
+		// Arrange
+		var type = new GeneratedMavlinkMessageFieldPrimitiveType("int", Original: null!);
+
+		// Act
+		Action act = () => _builder.BuildCondition("v", "FOO_BAR", type);
+
+		// Assert
+		act.Should().Throw<FormatException>()
+		   .WithMessage("The raw invalid value*FOO_BAR*");
+	}
+
+	[Theory]
+	[InlineData("nan")]
+	[InlineData("NAN")]
+	[InlineData("NaN")]
+	public void BuildCondition_ConstantName_IsCaseInsensitive(string token)
+	{
+		// Arrange
+		var type = new GeneratedMavlinkMessageFieldPrimitiveType("float", Original: null!);
+
+		// Act
+		var result = _builder.BuildCondition("val", token, type);
+
+		// Assert
+		result.Should().Be("!float.IsNaN(val)");
+	}
+}
